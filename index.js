36bde9cd603ad7e86bd2221e9c0a3aaf33de8911
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-09-12 21:45:36 -0400

    done

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// Create a call back function with battery batches?
+
+
+const totalBatteries = batteryBatches.reduce((total,batt)=> total + batt, 0)
+
+/* Think of it like 
+batteryBatches.
+function couponCounter(totalAmount, location) {
+  return totalAmount + location.amount;
+}
+and a starting point of 0 added on to the end */
